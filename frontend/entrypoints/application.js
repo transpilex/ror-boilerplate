ef0f13ca6f3e49226f639586cbe10c24e9ab87aa
Anant-Navadiya <anantnavadiya1530@gmail.com>
2025-08-14 12:48:57 +0530

    initial commit

---
+++ frontend/entrypoints/application.js
@@ -0,0 +1,9 @@
+// To see this message, follow the instructions for your Ruby framework.
+//
+// When using a plain API, perhaps it's better to generate an HTML entrypoint
+// and link to the scripts and stylesheets, and let Vite transform it.
+console.log('Vite ⚡️ Ruby')
+
+import '../scss/app.scss'
+
+import '../js/config'
